10908d07d0e413b6dd3cbfe0d1ec2cb920c3dc96
Viktoriya22 <vika.kup97@mail.ru>
2019-10-17 02:06:40 +0700

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include <QCoreApplication>
 #include "human.h"
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-    Human *h = new Human();
+   Human *h = new Human();
     h->say();
     Human *x = new Human("Petr",176,71);
     x->say();
-    return a.exec();
+    return 0;
 }
