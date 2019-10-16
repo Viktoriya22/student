23ab4e399807c66b0d86a9814198411c8365ef31
Viktoriya22 <56152257+Viktoriya22@users.noreply.github.com>
2019-10-17 00:08:18 +0700

    Добавлено  объявление  класса Human

---
+++ human.h
@@ -0,0 +1,16 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+
+
+class Human
+{
+public:
+    Human();
+    Human(char*,int,int);
+    void say();
+    char *name;
+    int rost;
+    float ves;
+};
+
+#endif // HUMAN_H
